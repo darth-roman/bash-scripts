ab04340e48567ee8f98bc16b88c8ab294f85dca8
Chouchane Abderrahmane <chouchane.louai20@gmail.com>
2025-08-09 19:46:54 +0100

    fix: printing results / messages now

--- gen-mrc.sh
+++ gen-mrc.sh
@@ -3,19 +3,25 @@ createController(){
     if ! [ -d "controllers" ]; then
         mkdir "controllers"
     fi
-    touch controllers/"$1Controller.$2"
+    local filename=controllers/"$1Controller.$2"
+    touch $filename
+    echo "$filename created successfully."
 }
 createModel(){
     if ! [ -d "models" ]; then
         mkdir "models"
     fi
-    touch models/"$1.$2"
+    local filename=models/"$1.$2"
+    touch $filename
+    echo "$filename created successfully. "
 }
 createRoutes(){
     if ! [ -d "routes" ]; then
         mkdir "routes"
     fi
-    touch routes/"$1Routes.$2"
+    local filename=routes/"$1Routes.$2"
+    touch $filename
+    echo "$filename created successfully. "
 }
 createController $1 $2
 createModel $1 $2
