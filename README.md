bfb073e69b4f94f669d279b7d771d0ccb83d4df2
Chouchane Abderrahmane <chouchane.louai20@gmail.com>
2025-08-11 16:29:42 +0100

    docs: Documenting the new command, fix: cleaned the code

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Bash Scripts
-[`gen-mrc`](gen-mrc.sh)
+
+## [`gen-mrc`](gen-mrc.sh)
 This is a tool to generate a model, a routing and a controller files, for `expressjs`(As intentded)
 
 - It checks if a folder `controllers` , `models` and `routes` exists, then creates the three files
 - If the directories do not exist, it creates them (**no shit sherlock**)
 
-## How to use:
+### How to use:
 - download | clone the repo\
 - add the clones directory to your `$PATH` variable
 - It is recommended to add an alias for easy use
@@ -22,3 +23,18 @@ Will generate these files:
 - `routes/UserRoutes.ts`
 
 **IMPORTANT**: This is just a 0.0.1 version, some improvements will be added with time.
+
+
+## [`gh-ssh-gen`](gh-ssh-gen.sh)
+Generates and creates new SSH Key in your github account
+
+
+### How to use
+```bash
+$> gh-ssh-gen <key-name> <passphrase> <email> <ssh-title>
+```
+**All Are required**
+
+**IMPORTANT**: Using this script will have to you use your own Personal Access Token, or Fine grained ones, just make sure you set SSH Key in permissions to read and write 
+
+[**More Information: Github Docs**](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-fine-grained-personal-access-token)
--- gh-ssh-gen.sh
+++ gh-ssh-gen.sh
@@ -9,47 +9,46 @@ GIT_FG_TOKEN="<YourFineGrainedAccessToken>"
 
 # URL of the request
 URL="https://api.github.com/user/keys"
-
-# echo "HOST: $HOST
-#       KEY_ID: $KEY_ID
-#       URL: $HOST/$user_keys
-#       -H "Authorization: Bearer $GIT_FG_TOKEN" \
-# "
-
-
+TITLE=$4
 
 public_key="$1.pub"
 private_key="$1"
 
-# generating the SSH key, 
-# -t is the type flag, rsa, dsa, rsa1 ...
-# github docs suggestes to set the type as ed25519
-# -N to set a passpharse
-# -C a comment (set your email here)
-# -f is the output file where it will be saved
-ssh-keygen -t ed25519 -N $2 -C "$3" -f "$1"
-
-# Moving both private  and public keys to ~/.ssh directory
-mv $1 ~/.ssh/
-mv $1.pub ~/.ssh/
 
 # Readind the content of the public key value
-file_content=$(cat ~/.ssh/$public_key)
+createLocalSSHKey(){
+    # generating the SSH key, 
+    # -t is the type flag, rsa, dsa, rsa1 ...
+    # github docs suggestes to set the type as ed25519
+    # -N to set a passpharse
+    # -C a comment (set your email here)
+    # -f is the output file where it will be saved
+    ssh-keygen -t ed25519 -N $2 -C "$3" -f "$1"
 
-# Sending a POST request with the title and key
-# DO NOT FORGET TO ESCAPE internal quotation marks
-# -H is for the headers
-# -d is for the actual data 
-curl -L \
-    -X POST \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GIT_FG_TOKEN" \
-    -H "X-Github-Api-Version: 2022-11-28" \
-    $URL \
-    -d "{\"title\":\"ssh-ed25519-darth\", \"key\":\"$file_content\"}"   
+    # Moving both private  and public keys to ~/.ssh directory
+    mv $1 ~/.ssh/
+    mv $1.pub ~/.ssh/
+}
+
+createGithubSSHKey(){
+    local file_content=$(cat ~/.ssh/$public_key)
+    # Sending a POST request with the title and key
+    # DO NOT FORGET TO ESCAPE internal quotation marks
+    # -H is for the headers
+    # -d is for the actual data 
+    curl -L \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $1" \
+        -H "X-Github-Api-Version: 2022-11-28" \
+        $2 \
+        -d "{\"title\":\"ssh-ed25519-$3\", \"key\":\"$file_content\"}"   
+}
+
+createLocalSSHKey $1 $2 $3
+createGithubSSHKey $GIT_FG_TOKEN $URL $TITLE
 
 # running ssh-agent
-
 eval "$(ssh-agent -s)"
 
 # adding the key to ssh-agent
